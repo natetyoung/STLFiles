cf834875a3b61edd55edaa8191f2ee3c6bb68629
natetyoung <natetyoung42@gmail.com>
2016-07-26 17:56:27 -0700

    More flexible MandelbrotSTL

--- src/mandelbrot/MandelbrotSTL.java
+++ src/mandelbrot/MandelbrotSTL.java
@@ -2,21 +2,32 @@ package mandelbrot;
 
 import java.io.IOException;
 
-import src.*;
+import src.BinSTLCreator;
+import src.Facet;
+import src.STLCreator;
+import src.Vec3;
 
 public class MandelbrotSTL {
 	public static void main(String[] args) throws IOException {
-		MQMath.MAX_ITER = 50;
-		STLCreator sc = new STLCreator("C:\\Users\\Nate\\Desktop2\\mb.stl","mandelblock");
-		Vec3[][] arr = new Vec3[100][100];
-		for(double x = -2; x<=1.995; x+=.04){
-			for(double y = -2; y<=1.995; y+=.04){
+		MQMath.MAX_ITER = 100;
+		double[] xr = {-.035,0};
+		double[] yr = {.70,.73};
+		double scale = 300/(xr[1]-xr[0]);
+		int xd = 300;
+		int yd = (int)(scale*(yr[1]-yr[0]));
+		double xi = (xr[1]-xr[0])/xd;
+		double yi = (yr[1]-yr[0])/yd;
+		STLCreator sc = new STLCreator("mandelbrot.stl","mandelbrot");
+		Vec3[][] arr = new Vec3[xd][yd];
+		for(double x = xr[0]; x<xr[1]-xi/2; x+=xi){
+			for(double y = yr[0]; y<yr[1]-yi/2; y+=yi){
 				double iter = MQMath.mIterations(new ComplexNum(x,y));
 				float height = 0;
 				if(iter>0)
-					height = (float) (Math.log(iter)+1)/5;
-				arr[(int)Math.round(((x+2)/.04))][(int)Math.round(((y+2)/.04))] = 
-						new Vec3((float)x,height,(float)y);
+					height = (float) (Math.log(Math.log(iter))+1)/3;
+				int[] coords = {(int)Math.round(((x-xr[0])/xi)),(int)Math.round(((y-yr[0])/yi))};
+				arr[coords[0]][coords[1]] = 
+						new Vec3(coords[0]/100f,height,coords[1]/100f);
 			}
 		}
 		for(int i=0; i<arr.length; i++)
@@ -24,15 +35,15 @@ public class MandelbrotSTL {
 				if(arr[i][j]==null) System.out.println(i+" "+j);
 		for(int i=0; i<arr.length-1; i++)
 			for(int j=0; j<arr[i].length-1; j++){
-				sc.addFacet(new Facet(arr[i][j],arr[i+1][j+1],arr[i+1][j]));
-				sc.addFacet(new Facet(arr[i][j],arr[i][j+1],arr[i+1][j+1]));
+				if(arr[i][j]!=null&&arr[i+1][j+1]!=null){
+					if(arr[i+1][j]!=null){
+						sc.addFacet(new Facet(arr[i][j],arr[i+1][j],arr[i+1][j+1]));
+					}
+					if(arr[i][j+1]!=null){
+						sc.addFacet(new Facet(arr[i][j],arr[i][j+1],arr[i+1][j+1]));
+					}
+				}
 			}
-		Vec3 c1 = new Vec3(-2,0,-2);
-		Vec3 c2 = new Vec3(-2,0,2);
-		Vec3 c3 = new Vec3(2,0,-2);
-		Vec3 c4 = new Vec3(2,0,2);
-		sc.addFacet(new Facet(c1, c4, c2));
-		sc.addFacet(new Facet(c4, c1, c3));
 		sc.finish();
 		System.out.println("Done");
 	}
